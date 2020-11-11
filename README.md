8cd38e9544d05bd98ca915ba84a7c912d2633e07
ppadkil <prasanna.padkil@gmail.com>
2020-11-11 16:12:38 +0530

    core winning logic, css changes and player name display

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # connect-four
 Developing a connect four game
+
+Libraries used:
+
+Jquery:
+
+1) For easy modification of DOM elements, like use of addClass, removeClass.
+2) Download the project and just run the index.html file to see the results
--- board.js
+++ board.js
@@ -1,51 +1,112 @@
+/* This class is used to create/ draw a connect four game board and logic for deciding winner*/
 class Board {
- constructor(boardElementId) {
-     // For 2 dimensional array and also the columns and rows should be dynamic as per requirement of game
-     this.totalRows = 6;
-     this.totalColumns = 7;
-     this.board = boardElementId;
-     this.createBoard();
-     // current player intitialized to Player 1
-     this.currentPlayer = 'Player 1';
-     
-     //handle column click and change player..
-     this.handleClickEvents();
- }
-
- createBoard() {
-    const $board = $(this.board);
-    for(let row = 0; row < this.totalRows; row++) {
-        const $row = $('<div>').addClass('row')
-        for (let col = 0; col < this.totalColumns; col++ ) {
-            const $col = $('<div>')
-            .addClass('col empty')
-            .attr('data-col', col).attr('data-row', row);
-            $row.append($col);
-        }
-        $board.append($row);
+    constructor(boardElementId) {
+        this.totalRows = 6;
+        this.totalColumns = 7;
+        this.board = boardElementId;
+        this.createBoard();
+        this.isGameOver = false;
+        this.playerOne = 'Player 1';
+        this.playerTwo = 'Player 2';
+        this.playerOneColor = 'player1-color';
+        this.playerTwoColor = 'player2-color';
+        // current player intitialized to Player 1
+        this.currentPlayer = this.playerOne;
+        //handle column click and change player..
+        this.handleClickEvents();
     }
- }
 
- handleClickEvents() {
-    const $board = $(this.board);
-    let that = this;
-    function getEmptyCell(column) {
-        const cells = $(`.col[data-col='${column}']`);
-        for (let i = cells.length - 1; i >= 0; i--) {
-            const $cell = $(cells[i]);
-            if($cell.hasClass('empty')) {
-                return $cell;
+    // Create a board with defined no of rows and columns using global totalRows & totalColumns variables.
+    createBoard() {
+        const $board = $(this.board);
+        for (let row = 0; row < this.totalRows; row++) {
+            const $row = $('<div>').addClass('row');
+            for (let col = 0; col < this.totalColumns; col++) {
+                const $col = $('<div>')
+                    .addClass('col empty')
+                    .attr('data-col', col).attr('data-row', row);
+                $row.append($col);
             }
-            return null;
+            $board.append($row);
         }
     }
-    
-    $board.on('click', '.col.empty', function() {
-        const currentColumn = $(this).data('col');
-        const $emptyCell = getEmptyCell(currentColumn);
-        that.currentPlayer === 'Player 1'?  $emptyCell.addClass('player1-color') : $emptyCell.addClass('player2-color');
-        that.currentPlayer = (that.currentPlayer === 'Player 1') ? 'Player 2':'Player 1';
-        console.log('Player Changed = ', that.currentPlayer);
-    })
- }
+
+    handleClickEvents() {
+        const $board = $(this.board);
+        let that = this;
+        function getEmptyCell(column) {
+            const cells = $(`.col[data-col='${column}']`);
+            console.log($(`.col[data-col='${column}']`));
+            for (let i = cells.length - 1; i >= 0; i--) {
+                const $cell = $(cells[i]);
+                if ($cell.hasClass('empty')) {
+                    return $cell;
+                }
+            }
+        }
+
+        $board.on('click', '.col.empty', function () {
+            const currentColumn = $(this).data('col');
+            const $emptyCell = getEmptyCell(currentColumn);
+            $emptyCell.removeClass('empty');
+            // add class for current player and store current player value
+            that.currentPlayer === that.playerOne ? 
+            $emptyCell.addClass(that.playerOneColor) : $emptyCell.addClass(that.playerTwoColor);
+            $emptyCell.data('player', that.currentPlayer);
+
+            // Find winner - core logic
+            const winner = that.checkWinner($emptyCell.data('row'), $emptyCell.data('col'));
+            if (winner) {
+                that.isGameOver = true;
+                alert(`Game won by ${winner} !!`);
+                return;
+            }
+
+            that.currentPlayer = (that.currentPlayer === that.playerOne) ? that.playerTwo : that.playerOne;
+            $('#currentPlayer').text(that.currentPlayer);
+            console.log('Player Changed = ', that.currentPlayer);
+        })
+    }
+
+    checkWinner(row, column) {
+        const that = this;
+        function $getCell(rowNo, colNo) {
+            return $(`.col[data-row='${rowNo}'][data-col='${colNo}']`);
+        }
+
+        function checkDirection(direction) {
+            let total = 0;
+            let rowNo = row + direction.row;
+            let colNo = column + direction.column;
+            let $next = $getCell(rowNo, colNo);
+            // TODO: instead of $getCell which is more on DOM manipulation, 
+            // this logic could be moved to a separate class to have, filled positions in a 2-dimensional array filledPositions[rowNo, colNo] = COLORS.RED
+            // Then using jquery .index(column) and .index(parent).column
+            while (rowNo > 0 && rowNo < that.totalRows && colNo > 0 && colNo < that.totalColumns
+                && $next.data('player') === that.currentPlayer) {
+                total++;
+                rowNo += direction.row;
+                colNo += direction.column;
+                $next = $getCell(rowNo, colNo);
+            }
+            return total;
+        }
+        function checkResult(directionUp, directionDown) {
+            let total = 1 + checkDirection(directionUp) + checkDirection(directionDown);
+            if (total >= 4) {
+                return that.currentPlayer;
+            } else {
+                return null;
+            }
+        }
+
+        function checkVerticals() {
+            return checkResult({ row: -1, column: 0 }, { row: 1, column: 0 });
+        }
+        function checkHorizontals() {
+            return checkResult({ row: 0, column: -1 }, { row: 0, column: 1 });
+        }
+       
+        return checkVerticals() || checkHorizontals(); 
+    }
 }
