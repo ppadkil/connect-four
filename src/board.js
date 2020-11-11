83526f668c6289beacbef625d3fc159684065b3d
ppadkil <prasanna.padkil@gmail.com>
2020-11-12 02:45:20 +0530

    optimized version presentation layer DOM manipualtion and actual logic separated

--- src/board.js
+++ src/board.js
@@ -1,133 +1,80 @@
 /* This class is used to create/ draw a connect four game board and logic for deciding winner*/
 class Board {
     constructor(boardElementId) {
-        this.totalRows = 6;
-        this.totalColumns = 7;
         this.board = boardElementId;
+        this.boardState = new BoardState();
         this.createBoard();
         this.isGameOver = false;
-        this.playerOne = 'Player 1';
-        this.playerTwo = 'Player 2';
         this.playerOneColor = 'player1-color';
         this.playerTwoColor = 'player2-color';
-        // current player intitialized to Player 1
-        this.currentPlayer = this.playerOne;
         //handle column click and change player..
         this.handleClickEvents();
+
     }
 
     // Create a board with defined no of rows and columns using global totalRows & totalColumns variables.
     createBoard() {
-        this.isGameOver = false;
         const $board = $(this.board);
-        for (let row = 0; row < this.totalRows; row++) {
+        for (let row = 0; row < this.boardState.totalRows; row++) {
             const $row = $('<div>').addClass('row');
-            for (let col = 0; col < this.totalColumns; col++) {
+            for (let col = 0; col < this.boardState.totalColumns; col++) {
                 const $col = $('<div>')
                     .addClass('col empty')
-                    .attr('data-col', col).attr('data-row', row);
                 $row.append($col);
             }
             $board.append($row);
         }
+
+        $('#startGame').on('click', this.resetBoard.bind(this));
+    }
+
+    resetBoard() {
+        this.isGameOver = false;
+        this.boardState = new BoardState();
+        const $board = $(this.board);
+        $board.empty();
+        $("#gameStatus").addClass('hidden');
+        this.createBoard();
     }
 
     handleClickEvents() {
         const $board = $(this.board);
         let that = this;
-        function getEmptyCell(column) {
-            const cells = $(`.col[data-col='${column}']`);
-            for (let i = cells.length - 1; i >= 0; i--) {
-                const $cell = $(cells[i]);
-                if ($cell.hasClass('empty')) {
-                    return $cell;
-                }
-            }
+        function updateEmptyCellClass(currentColumn) {
+
+            const latestEmptyPostion = that.boardState.getLatestEmptyPosition(currentColumn);
+
+            const row$ = $(`.row:nth-child(${latestEmptyPostion[0] + 1})`);
+            const cell$ = row$.find(".col")[latestEmptyPostion[1]];
+
+            $(cell$).removeClass('empty');
+            let className = that.boardState.currentColor === that.boardState.COLORS.RED ? that.playerOneColor
+                : that.playerTwoColor;
+            $(cell$).addClass(className);
+
+
         }
 
         $board.on('click', '.col.empty', function () {
             if (that.isGameOver) {
                 return;
             }
-            const currentColumn = $(this).data('col');
-            const $emptyCell = getEmptyCell(currentColumn);
-            $emptyCell.removeClass('empty');
-            // add class for current player and store current player value
-            that.currentPlayer === that.playerOne ? 
-            $emptyCell.addClass(that.playerOneColor) : $emptyCell.addClass(that.playerTwoColor);
-            $emptyCell.data('player', that.currentPlayer);
+            const currentColumn = $(this).index();
+            updateEmptyCellClass(currentColumn);
+            that.boardState.updateLatestEmptyPosition(currentColumn);
+            const winner = that.boardState.checkWinner(currentColumn);
 
-            // Find winner - core logic..settimeout added so that last cell gets time to render.
-            setTimeout(() => {
-            const winner = that.checkWinner($emptyCell.data('row'), $emptyCell.data('col'));
+            // Find winner - core logic
             if (winner) {
+                $('#currentPlayerStatus').html(`Player ${that.boardState.currentColor}`);
+                $("#gameStatus").toggleClass('hidden');
                 that.isGameOver = true;
-                alert(`Game won by ${winner} !!`);
-                // Reset board for a new game and return
-                that.resetBoard();
                 return;
             }
-            that.currentPlayer = (that.currentPlayer === that.playerOne) ? that.playerTwo : that.playerOne;
-            $('#currentPlayer').text(that.currentPlayer);
-            }, 0);
-
+            if (!that.isGameOver) {
+                that.boardState.switchPlayer();
+                $('#currentPlayerAction').html(`Player ${that.boardState.currentColor}`);
+            }
         })
-    }
-
-    resetBoard() {
-        let that = this;
-        that.currentPlayer = that.playerOne;
-        const $board = $(that.board);
-        $board.empty();
-        that.createBoard();
-    }
-
-    checkWinner(row, column) {
-        const that = this;
-        function $getCell(rowNo, colNo) {
-            return $(`.col[data-row='${rowNo}'][data-col='${colNo}']`);
-        }
-
-        function checkDirection(direction) {
-            let total = 0;
-            let rowNo = row + direction.row;
-            let colNo = column + direction.column;
-            let $next = $getCell(rowNo, colNo);
-            // TODO: instead of $getCell which is more on DOM manipulation, 
-            // this logic could be moved to a separate class to have, filled positions in a 2-dimensional array filledPositions[rowNo, colNo] = COLORS.RED
-            // Then using jquery .index(column) and .index(parent).column
-            while (rowNo >= 0 && rowNo < that.totalRows && colNo >= 0 && colNo < that.totalColumns
-                && $next.data('player') === that.currentPlayer) {
-                total++;
-                rowNo += direction.row;
-                colNo += direction.column;
-                $next = $getCell(rowNo, colNo);
-            }
-            return total;
-        }
-        function checkResult(directionUp, directionDown) {
-            let total = 1 + checkDirection(directionUp) + checkDirection(directionDown);
-            if (total >= 4) {
-                return that.currentPlayer;
-            } else {
-                return null;
-            }
-        }
-
-        function checkVerticals() {
-            return checkResult({ row: -1, column: 0 }, { row: 1, column: 0 });
-        }
-        function checkHorizontals() {
-            return checkResult({ row: 0, column: -1 }, { row: 0, column: 1 });
-        }
-
-        function checkDiagonalsBottomToTop() {
-            return checkResult({ row: 1, column: -1 }, { row: 1, column: 1 });
-        }
-        function checkDiagonalsTopToBottom() {
-            return checkResult({ i: 1, j: 1 }, { i: -1, j: -1 });
-        }
-       
-        return checkVerticals() || checkHorizontals() || checkDiagonalsBottomToTop() || checkDiagonalsTopToBottom();
     }
 }
---
+++ src/boardState.js
@@ -0,0 +1,98 @@
+class BoardState {
+    constructor() {
+        this.totalRows = 6;
+        this.totalColumns = 7;
+        this.COLORS = {
+            RED: 1,
+            BLUE: 2
+        }
+        this.boardState = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ];
+        this.latestEmptyPositions = {
+            0: [5, 0],
+            1: [5, 1],
+            2: [5, 2],
+            3: [5, 3],
+            4: [5, 4],
+            5: [5, 5],
+            6: [5, 6]
+        }
+        this.currentColor = this.COLORS.RED;
+    }
+
+    switchPlayer() {
+        this.currentColor = (this.currentColor === this.COLORS.RED)
+            ? this.COLORS.BLUE : this.COLORS.RED;
+    }
+
+    checkWinner(currentColumn) {
+        const columnPosition = this.getLatestEmptyPosition(currentColumn);
+
+        const row = columnPosition[0] + 1;
+        const column = currentColumn;
+        const that = this;
+
+        function checkDirection(direction) {
+            let total = 0;
+            let rowNo = row + direction.row;
+            let colNo = column + direction.column;
+            let color = 0;
+            if (that.boardState[rowNo] && that.boardState[rowNo][colNo]) {
+                color = that.boardState[rowNo][colNo];
+            }
+
+            while (rowNo >= 0 && rowNo < that.totalRows && colNo >= 0 && colNo < that.totalColumns
+                && color === that.currentColor) {
+                total++;
+                rowNo += direction.row;
+                colNo += direction.column;
+                if (that.boardState[rowNo] && that.boardState[rowNo][colNo]) {
+                    color = that.boardState[rowNo][colNo];
+                }
+            }
+
+            return total;
+        }
+        function checkResult(directionUp, directionDown) {
+            let total = 1 + checkDirection(directionUp) + checkDirection(directionDown);
+            if (total >= 4) {
+                return that.currentColor;
+            } else {
+                return null;
+            }
+        }
+
+        function checkVerticals() {
+            return checkResult({ row: -1, column: 0 }, { row: 1, column: 0 });
+        }
+        function checkHorizontals() {
+            return checkResult({ row: 0, column: -1 }, { row: 0, column: 1 });
+        }
+
+        function checkDiagonalsBottomToTop() {
+            return checkResult({ row: 1, column: -1 }, { row: 1, column: 1 });
+        }
+        function checkDiagonalsTopToBottom() {
+            return checkResult({ i: 1, j: 1 }, { i: -1, j: -1 });
+        }
+
+        return checkVerticals() || checkHorizontals() || checkDiagonalsBottomToTop() || checkDiagonalsTopToBottom();
+    }
+
+    updateLatestEmptyPosition(currentColumn) {
+        const cellPosition = this.latestEmptyPositions[currentColumn];
+        const row = cellPosition[0];
+        this.boardState[row][currentColumn] = this.currentColor;
+        cellPosition[0] = cellPosition[0] - 1;
+    }
+
+    getLatestEmptyPosition(column) {
+        return this.latestEmptyPositions[column];
+    }
+}
